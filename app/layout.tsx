172429c4d5199abba00d993880978d3f78375ea1
SatyajeetM07 <mazumdersatyajeet@gmail.com>
2023-12-10 23:12:06 +0530

    commit 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,28 @@
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
-import Sidebar from '@/components/Sidebar'
-import './globals.css'
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Spotify',
-  description: 'Listen to music!',
-}
+  title: "Spotify",
+  description: "Listen to music!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
-  )
+  );
 }
